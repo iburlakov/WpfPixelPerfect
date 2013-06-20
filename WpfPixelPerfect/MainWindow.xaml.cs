0189fbe802a8795f946b634dce8247f694ce3019
iburlakov <iburlakov@gmail.com>
2013-06-20 12:07:28 +0400

    Added version indicator

--- WpfPixelPerfect/MainWindow.xaml.cs
+++ WpfPixelPerfect/MainWindow.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 using System.Windows.Forms;
 
@@ -79,6 +70,11 @@ namespace WpfPixelPerfect
             this.screen.MouseLeftButtonDown += (o, e) => this.DragMove();
             this.controlPanel.MouseLeftButtonDown += (o, e) => this.DragMove();
             this.close.Click += (o, e) => this.Close();
+        }
+
+        public string Version
+        {
+            get { return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(2); }
         }
     }
 }
